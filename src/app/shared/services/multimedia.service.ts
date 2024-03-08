dcaaa9daadbf2be77e8bc0496737580e8835b5c0
Eduardo347387 <143133753+Eduardo347387@users.noreply.github.com>
2024-03-07 20:06:02 -0600

    Implentacion de reproductor de audio

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -10,6 +10,10 @@ export class MultimediaService {
 
   callback: EventEmitter<any> = new EventEmitter<any>()
   public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined);
+  public timeElapsed$: BehaviorSubject<string> = new BehaviorSubject('00:00');
+  public timeRemaining$: BehaviorSubject<string> = new BehaviorSubject('00:00');
+  public playerStatus$: BehaviorSubject<string> = new BehaviorSubject('paused');
+  
   public audio!: HTMLAudioElement;
 
   constructor() { 
@@ -19,17 +23,61 @@ export class MultimediaService {
       next: responseOk => {
         if (responseOk) {
           this.setAudio(responseOk)
+          console.log(responseOk)
         }
       },
       error: e=>{
         console.log(e)
       }
     })
+
+    this.listenAllEvents()
+  }
+
+  private listenAllEvents(): void{
+    this.audio.addEventListener('timeupdate', this.calcularTime, false)
+    this.audio.addEventListener('playing', this.setPlayerStatus, false)
+    this.audio.addEventListener('play', this.setPlayerStatus, false)
+    this.audio.addEventListener('pause', this.setPlayerStatus, false)
+    this.audio.addEventListener('end', this.setPlayerStatus, false)
+  }
+
+  private setPlayerStatus = () => {
+    
+  }
+
+  private calcularTime = ():void =>{
+    const { duration, currentTime } = this.audio
+    this.setTimeElapsed(currentTime)
+    this.setRemainig(currentTime,duration)
+    console.table([duration, currentTime])
+    
+  }
+
+
+  private setTimeElapsed(currentTime:number):void {
+    let seconds = Math.floor(currentTime % 60)
+    let minutes = Math.floor((currentTime / 60) % 60)
+
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes
+    const displayFormat = `${displayMinutes}:${displaySeconds}`
+    this.timeElapsed$.next(displayFormat)
+  }
+
+  private setRemainig(currentTime: number, duration: number): void{
+    let timeLeft = duration - currentTime
+    let seconds = Math.floor(timeLeft % 60)
+    let minutes = Math.floor((timeLeft / 60) % 60) 
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes
+    const displayFormat = `-${displayMinutes}:${displaySeconds}`
+    this.timeRemaining$.next(displayFormat)
   }
 
   public setAudio(track: TracksModel): void {
     this.audio.src = track.url
-    console.log(track.url)
+    console.log( this.audio.src)
     this.audio.play() 
  
   } 
